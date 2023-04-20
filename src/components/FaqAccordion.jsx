bfe3bf7529de681a88499c2da0b714011e10fbaa
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2023-04-20 09:34:28 +0530

    Footer patch 1 (#16)
    * fix: add key prop
    
    * feat: create footer

--- src/components/FaqAccordion.jsx
+++ src/components/FaqAccordion.jsx
@@ -2,7 +2,11 @@ import { faqs } from "../../data/faq";
 import FaqAccordionItem from "./FaqAccordionItem";
 
 const FaqAccordion = () => {
-  return faqs.map((faq) => <FaqAccordionItem faq={faq} key={null} />);
+
+  return faqs.map((faq) => <FaqAccordionItem faq={faq} key={null}/>);
+
+
+
 };
 
 export default FaqAccordion;
