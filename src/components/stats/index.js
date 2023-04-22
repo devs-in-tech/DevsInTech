baef4297cf8f3ca22cb7d97fe56bdd8086c2c57c
Abhi Patel <76242769+AbhiPatel10@users.noreply.github.com>
2023-04-22 09:52:16 +0530

    Stats Component (#17)
    * feat: stats structure added
    
    * perf: change class to classname
    
    * perf: console remove

---
+++ src/components/stats/index.js
@@ -0,0 +1,23 @@
+const Stats = ({ stats }) => {
+  return (
+    <section className="text-gray-600 body-font">
+      <div className="container px-5 py-14 mx-auto">
+        <div className="flex flex-wrap -m-4 text-center">
+          {stats &&
+            stats.map((data, index) => {
+              return (
+                <div key={index} className="p-4 sm:w-1/3 w-full">
+                  <h2 className="title-font font-medium sm:text-4xl text-3xl text-transparent bg-clip-text bg-gradient-to-br from-[#FF008A] to-[#6100FF]">
+                    {data.number}+
+                  </h2>
+                  <p className="leading-relaxed text-white mt-2">{data.name}</p>
+                </div>
+              );
+            })}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Stats;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,10 @@
 import Navbar from "@/components/Navbar";
 import Head from "next/head";
 import Script from "next/script";
-export default function Home() {
+import About from "./about";
+import Home from "./home";
+
+export default function Main() {
   return (
     <>
       <Head>
@@ -9,8 +12,16 @@ export default function Home() {
         <meta name="description" content="Devs In Tech Community website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></Script>
-      <Script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></Script>
+      <Home />
+      <About />
+      <Script
+        type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"
+      ></Script>
+      <Script
+        nomodule
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"
+      ></Script>
       <Navbar />
     </>
   );
