21f44f893622ef0742fd13d9480b57f4aa26b99a
Abhi Patel <76242769+AbhiPatel10@users.noreply.github.com>
2023-04-25 21:46:09 +0530

    feat: about community commponent completed and document.js file added for fonts (#27)

--- src/components/aboutCommunity/index.js
+++ src/components/aboutCommunity/index.js
@@ -19,6 +19,16 @@ const AboutCommunity = () => {
   return (
     <>
       <Header name="What is DevsInTech Community?" />
+      <p className="my-10 text-justify font-secondary">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. . Lorem ipsum dolor
+        sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+        ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua.
+      </p>
       <Stats stats={stats} />
     </>
   );
