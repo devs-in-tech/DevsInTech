48b709d507902ed8f7e0d7c10cec14fa2583ffab
himanshu1221 <himanshuchhatwal9295@gmail.com>
2023-04-26 08:46:06 +0530

    fix: fixed images

--- src/components/Events_Cards/data.js
+++ src/components/Events_Cards/data.js
@@ -1,6 +1,8 @@
+import img1 from './images/1.jpg'
+
 export default [
     {
-        img: "/images/1.jpg",
+        img: img1,
         title: "Deep diving into API with POSTMAN",
         text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
     },
