207fa46a8d9660cf9fe275f169b4b3ee960163e7
Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>
2023-04-26 08:48:40 +0530

    feat: added event cards (#28)
    * feat: added event cards
    
    * fix: fixed images

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,11 @@
 import { AboutCommunity } from "@/components";
+import Cards from "@/components/Events_Cards/Cards";
 
 const Home = () => {
   return (
     <>
       <AboutCommunity />
+      <Cards />
     </>
   );
 };
