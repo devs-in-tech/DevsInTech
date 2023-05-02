74ef5856eb7bf8e4e12daabb63a5aeb7572b8a8e
aadarsh805 <aadarshthakur80501@gmail.com>
2023-05-02 16:33:12 +0530

    fix: made the suggested changes to solve problem

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -5,12 +5,16 @@ const Button = (props) => {
   return (
     <>
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 210e75f (fix: made the suggested changes)
       <Link target="_blank" href={props.url}>
         <button className="bg-[#194fbc] gap-2 flex items-center hover:bg-[#002775 ] text-white font-bold py-2 px-4 rounded relative">
           <span className="absolute top-0 left-0 right-0 h-1  rounded-t-md bg-gradient-to-b from-white to-[#194fbc] opacity-25"></span>
           <span>{props.name}</span>
           <HiCheckCircle />
         </button>
+<<<<<<< HEAD
 =======
       <Link
         target="_blank"
@@ -21,6 +25,8 @@ const Button = (props) => {
         <span>{props.name}</span>
         <HiCheckCircle />
 >>>>>>> ecbb43b (fix: solve main branch merge problem)
+=======
+>>>>>>> 210e75f (fix: made the suggested changes)
       </Link>
     </>
   );
