f0ef5cdeb364c2ed1473ea1f63574152918f90f1
Abhi Patel <mr.abhiofficial100@gmail.com>
2023-05-03 18:22:29 +0530

    fix: solve basic problems

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -2,13 +2,13 @@ import Button from "../button";
 
 const HeroSection = () => {
   return (
-    <div className="min-h-[calc(100vh-250px)] flex items-center bg-black mt-20">
-      <div className="flex items-center justify-center text-center md:items-start md:text-left md:justify-start md:px-32 px-7">
+    <div className="min-h-[calc(100vh-115px)] flex items-center bg-black">
+      <div className="flex items-center justify-center text-center md:items-start md:text-left md:justify-start px-7">
         <div className="flex flex-col gap-12 md:w-2/3 w-full">
-          <h1 className="hero-heading md:text-7xl text-6xl font-medium">
+          <h1 className="md:text-7xl text-6xl font-primary font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2]">
             Devs In Tech Community
           </h1>
-          <p className="md:text-2xl text-xl text-white">
+          <p className="md:text-2xl font-secondary text-xl text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. . Lorem
             ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
