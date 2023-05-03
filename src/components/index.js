e8b0a1ca6cb80e9b46a5c8488c7940956d334d94
aadarsh805 <aadarshthakur80501@gmail.com>
2023-05-03 13:04:12 +0530

    feat: added hero section

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
-export { default as header } from "./header";
 export { default as Hello } from "./Hello";
+export { default as HeroSection } from "./HeroSection";
 export { default as AboutCommunity } from "./HomePage/aboutCommunity";
 export { default as stats } from "./HomePage/stats";
+export { default as header } from "./header";
