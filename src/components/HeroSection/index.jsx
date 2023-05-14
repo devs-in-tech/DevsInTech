fe0183524b9a6a0c82b8a65591b8d1c587f1ccc1
Abhi Patel <mr.abhiofficial100@gmail.com>
2023-05-14 14:50:59 +0530

    fix: responsive hero section

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -5,10 +5,10 @@ const HeroSection = () => {
     <div className="min-h-[calc(100vh-115px)] flex items-center bg-black">
       <div className="flex items-center justify-center text-center md:items-start md:text-left md:justify-start px-7">
         <div className="flex flex-col items-center md:items-start gap-12 md:w-2/3 w-full">
-          <h1 className="md:text-7xl text-6xl font-primary font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2]">
+          <h1 className="md:text-7xl sm:text-6xl text-4xl font-primary font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2]">
             Devs In Tech Community
           </h1>
-          <p className="md:text-2xl font-secondary text-xl text-white">
+          <p className="md:text-2xl font-secondary sm:text-xl text-lg text-white sm:text-start text-justify">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua. . Lorem
             ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -16,7 +16,7 @@ const HeroSection = () => {
             dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
             incididunt ut labore et dolore magna aliqua.
           </p>
-          <div className="flex gap-8 items-center justify-center md:justify-start">
+          <div className="flex flex-wrap gap-8 items-center justify-center md:justify-start">
             <Button
               name="Join Discord"
               url="https://discord.com/invite/g7FmxB9uZp "
