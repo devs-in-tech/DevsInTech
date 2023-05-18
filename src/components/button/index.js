8396177b1c292063aa097537cf8869a29e8e24b7
Jay Kumar Gupta <100681165+jayk-gupta@users.noreply.github.com>
2023-05-18 20:32:36 +0530

    added hover bg color on buttons

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -4,7 +4,7 @@ import { HiCheckCircle } from "react-icons/hi";
 const Button = (props) => {
   return (
     <Link target="_blank" href={props.url}>
-      <button className="bg-[#194fbc] gap-2 flex items-center hover:bg-[#002775 ] text-white font-bold py-2 px-4 rounded relative">
+      <button className="bg-[#194fbc] gap-2 flex items-center hover:bg-[#143f96] text-white font-bold py-2 px-4 rounded relative">
         <span className="absolute top-0 left-0 right-0 h-1 rounded-t-md bg-gradient-to-b from-white to-[#194fbc] opacity-25"></span>
         <span className="whitespace-nowrap ">{props.name}</span>
         <HiCheckCircle />
