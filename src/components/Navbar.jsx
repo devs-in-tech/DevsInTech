b2fd675dbfe1e2d680a406fa9b7f352a454d59c1
Arin Paliwal <paliwalarin09@gmail.com>
2023-05-21 17:07:04 +0530

    Added Nav Hover as theme and added button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,23 +13,26 @@ const Navbar = () => {
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className="w-full top-0 left-0">
+    <div className="w-full top-0 left-0 sticky bg-black">
       <div className="md:flex md:items-center md:justify-between py-4 px-7">
-        <div
-          className="font-bold text-base cursor-pointer flex flex-col md:items-center font-secondary 
-      text-white"
-        >
+        <div className="flex items-center">
           <span>
-            <Image className="h-14 w-14" src={Logo} alt="" />
+            <Image className="w-20" src={Logo} alt="" />
           </span>
-          DevsInTech
+          <div className="font-bold text-base cursor-pointer ml-2 text-white font-secondary">
+            DevsInTech
+          </div>
         </div>
 
         <ul
-          className={`md:flex md:items-center md:pb-0 px-20 md:px-0 pb-12 md:justify-center absolute md:static md:z-auto left-0 w-full transition-all duration-500 ease-in ${
-            open ? "top-24  bg-black" : "top-[-550px]"
-          }`}
+          className={`md:flex md:items-center md:pb-0 px-20 md:px-0 pb-12 md:justify-end absolute md:static md:z-auto right-0 w-full transition-all duration-500 ease-in ${open ? "top-24  bg-black" : "top-[-550px]"
+            }`}
         >
+          <Button
+            name="Join Us"
+            url="https://discord.com/invite/g7FmxB9uZp"
+            className="md:block hidden ml-10"
+          />
           {Links.map((link) => (
             <li
               key={link.name}
@@ -37,28 +40,20 @@ const Navbar = () => {
             >
               <Link
                 href={link.link}
-                className="text-white hover:text-gray-400 duration-500"
+                className="text-white hover:text-[#194fbc] duration-500"
               >
                 {link.name}
               </Link>
             </li>
           ))}
-          <span className="md:hidden block">
-            <Button
-              name="Join Us"
-              url="https://discord.com/invite/g7FmxB9uZp"
-            />
-          </span>
         </ul>
-        <span className="md:ml-auto md:block hidden">
-          <Button name="Join Us" url="https://discord.com/invite/g7FmxB9uZp " />
-        </span>
-      </div>
-      <div
-        onClick={() => setOpen(!open)}
-        className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-white"
-      >
-        <ion-icon name={open ? "close" : "menu"}></ion-icon>
+
+        <div
+          onClick={() => setOpen(!open)}
+          className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-white"
+        >
+          <ion-icon name={open ? "close" : "menu"}></ion-icon>
+        </div>
       </div>
     </div>
   );
