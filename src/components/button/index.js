0047e75250ee4cd878e4ec6582a768757d4de87e
Tushar98644 <b.toshar@iitg.ac.in>
2023-05-22 20:28:08 +0530

    feat: improve button ui

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,76 +1,62 @@
 import React from "react";
 import { FaCheckCircle } from "react-icons/fa";
-import styled, { keyframes } from "styled-components";
 import { motion } from "framer-motion";
-
-const GradientButton = styled(motion.a)`
-  display: inline-flex;
-  align-items: center;
-  background: linear-gradient(to right, #FF6666, blue);
-  color: white;
-  font-weight: bold;
-  padding: 8px 16px;
-  border-radius: 4px;
-  position: relative;
-  cursor: pointer;
-  text-decoration: none;
-  overflow: hidden;
-  transition: transform 0.3s, box-shadow 0.3s;
-
-  &:hover {
-    transform: translateY(-2px);
-    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.1);
-  }
-`;
-
-const GradientLine = styled.span`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 2px;
-  border-radius: 2px;
-  background: linear-gradient(to right, red, #143f96);
-  transform: scaleX(0);
-  transform-origin: left;
-  transition: transform 0.3s;
-`;
-
-const ButtonText = styled.span`
-  white-space: nowrap;
-`;
-
-const IconBounce = keyframes`
-  0% {
-    transform: translateY(0);
-  }
-  50% {
-    transform: translateY(-4px);
-  }
-  100% {
-    transform: translateY(0);
-  }
-`;
-
-const Icon = styled(FaCheckCircle)`
-  margin-left: 8px;
-  animation: ${IconBounce} 1s infinite;
-`;
 
 const Button = (props) => {
   const variants = {
-      scale: 1.1,
-      transition: {
-        duration: 0.3,
-        yoyo: Infinity,
+    scale: 1.1,
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
     },
   };
+
+  const buttonStyle = {
+    display: "inline-flex",
+    alignItems: "center",
+    background: "linear-gradient(to right, #FF6666, blue)",
+    color: "white",
+    fontWeight: "bold",
+    padding: "8px 16px",
+    borderRadius: "4px",
+    position: "relative",
+    cursor: "pointer",
+    textDecoration: "none",
+    overflow: "hidden",
+    transition: "transform 0.3s, box-shadow 0.3s",
+  };
+
+  const lineStyle = {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    height: "2px",
+    borderRadius: "2px",
+    background: "linear-gradient(to right, red, #143f96)",
+    transform: "scaleX(0)",
+    transformOrigin: "left",
+    transition: "transform 0.3s",
+  };
+
   return (
-    <GradientButton whileHover={variants} href={props.url} target="_blank" rel="noopener noreferrer">
-      <GradientLine />
-      <ButtonText>{props.name}</ButtonText>
-      <Icon />
-    </GradientButton>
+    <motion.a
+      whileHover={variants}
+      href={props.url}
+      target="_blank"
+      rel="noopener noreferrer"
+      style={buttonStyle}
+    >
+      <span style={lineStyle}></span>
+      <span style={{ whiteSpace: "nowrap" }}>{props.name}</span>
+      <motion.span
+        style={{ marginLeft: "8px" }}
+        animate={{ y: [0, -4, 0] }}
+        transition={{ duration: 1, repeat: Infinity }}
+      >
+        <FaCheckCircle />
+      </motion.span>
+    </motion.a>
   );
 };
 
