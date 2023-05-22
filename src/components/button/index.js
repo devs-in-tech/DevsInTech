07238714f61478668a7395479a71ba08aa6daab7
manasi-jain <jain.manasi06@gmail.com>
2023-05-22 11:35:55 +0530

    Added icons to the button component

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,14 +1,17 @@
 import Link from "next/link";
+import { FaDiscord, FaTwitter } from "react-icons/fa";
 import { HiCheckCircle } from "react-icons/hi";
 
 const Button = (props) => {
   return (
     <Link target="_blank" href={props.url}>
-      <button className="bg-[#194fbc] gap-2 flex items-center hover:bg-[#143f96] text-white font-bold py-2 px-4 rounded relative">
+      <buttons className="bg-[#194fbc] gap-2 flex items-center hover:bg-[#143f96] text-white font-bold py-2 px-4 rounded relative">
         <span className="absolute top-0 left-0 right-0 h-1 rounded-t-md bg-gradient-to-b from-white to-[#194fbc] opacity-25"></span>
         <span className="whitespace-nowrap ">{props.name}</span>
-        <HiCheckCircle />
-      </button>
+        {props.name === "Join Discord" && <FaDiscord className="mr-2" />}
+        {props.name === "Follow on Twitter" && <FaTwitter className="mr-2" />}
+        {props.name === "Join Us" && <HiCheckCircle />}
+      </buttons>
     </Link>
   );
 };
