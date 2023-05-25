02c9568a3aefbc32931e7e35e2ca2604ff367d16
Mohammad Shaad Shaikh <shaikh.mohammad2021@vitstudent.ac.in>
2023-05-25 18:53:13 +0530

    feat: add counter effect (#146)
    * feat: add counter effect
    
    * feat: add counter effect with increased speed

--- src/components/HomePage/stats/index.jsx
+++ src/components/HomePage/stats/index.jsx
@@ -1,4 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
 const Stats = ({ stats }) => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => prevCount + 1);
+    }, 10); // Adjust the interval duration as needed
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-4 mx-auto">
@@ -8,7 +22,7 @@ const Stats = ({ stats }) => {
               return (
                 <div key={index} className="p-4 sm:w-1/3 w-full">
                   <h2 className="title-font font-medium sm:text-4xl text-3xl text-transparent bg-clip-text bg-gradient-to-br from-[#FF008A] to-[#6100FF]">
-                    {data.number}+
+                    {count > data.number ? data.number : count}+
                   </h2>
                   <p className="leading-relaxed text-white mt-2">{data.name}</p>
                 </div>
