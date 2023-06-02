122b553af9d566e0553042d4e0fb040ade6add80
Jay-Thoriya <120302342+Jay-Thoriya@users.noreply.github.com>
2023-06-02 20:04:53 +0530

    feat: scrolling up button top of cards
    - https://github.com/devs-in-tech/DevsInTech/issues/188
    - In the mobile view scrolling up button is go top of the cards.

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -26,7 +26,7 @@ const ScrollToTop = () => {
   return (
     scroll && (
       <div
-        className="fixed cursor-pointer bg-[#13161B] bottom-20 right-10 p-4 rounded-full shadow-lg text-white hover:bg-gradient-to-br from-[#FF008A] to-[#6100FF] transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl"
+        className="fixed cursor-pointer zIndex bg-[#13161B] bottom-20 right-10 p-4 rounded-full shadow-lg text-white hover:bg-gradient-to-br from-[#FF008A] to-[#6100FF] transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110 hover:shadow-xl"
         onClick={scrollToTop}
       >
         <FiArrowUp size={28} />
