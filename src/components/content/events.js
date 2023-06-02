cdb6eb2293585a2d8051320c8214a5b892729530
Abhi Patel <mr.abhiofficial100@gmail.com>
2023-06-02 13:00:14 +0530

    feat: new events page added

--- src/components/content/events.js
+++ src/components/content/events.js
@@ -1,24 +1,38 @@
 import API from "../../assets/Event/API.jpg";
 import Built_for_deveelopers from "../../assets/Event/Built_for_deveelopers.jpg";
+import Devops_Kaiwalya from "../../assets/Event/Devops_Kaiwalya.png";
 import Susmita_hands_on_project from "../../assets/Event/Susmita_hands_on_project.jpg";
+import TapasSir_Image from "../../assets/Event/Tapas_sir.png";
 
 const data = [
   {
-    key: '1',
+    key: "1",
+    img: Devops_Kaiwalya,
+    title: "Introduction to world of Devops",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    key: "2",
     img: API,
     title: "Deep diving into API with POSTMAN",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    key: '2',
+    key: "3",
     img: Built_for_deveelopers,
     title: "Five Tips to crack the Front-End Role at FAANG",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
   {
-    key: '3',
+    key: "4",
     img: Susmita_hands_on_project,
     title: "Introduction to Next.js and Tailwind CSS with hands-on project",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
+  },
+  {
+    key: "5",
+    img: TapasSir_Image,
+    title: "Exclusive Session",
     text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ab eius molestias, quasi, quam omnis magnam impedit hic totam repudiandae reiciendis voluptate inventore nisi quidem.",
   },
 ];
