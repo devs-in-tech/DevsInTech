9c418de63a15e4e50c688a70bda8354422d17248
Pranav Tripathi <111558974+prnvtripathi@users.noreply.github.com>
2023-06-05 14:49:30 +0530

    feat: Add testimonial carousel (#31)
    * feat: Create testimonial carousel
    
    * feat: add testimonial carousel
    
    * feat(update): update in testimonials
    
    * feat(update): update in testimonials
    
    * style: change in design of carousel
    
    * fix: add navigation buttons
    
    * style: added responsiveness to the carousel
    
    * fix: fixed unnecessary code
    
    * feature(header): Create Header of the component
    
    ---------
    
    Co-authored-by: Abhi Patel <76242769+AbhiPatel10@users.noreply.github.com>
    Co-authored-by: Himanshu Chhatwal <himanshuchhatwal9295@gmail.com>

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -3,6 +3,7 @@ import FaqAccordion from "@/components/FaqAccordion";
 import Footer from "@/components/Footer";
 import Cards from "@/components/HomePage/Events_Cards";
 import Navbar from "@/components/Navbar";
+import TestimonialCarousel from "@/components/testimonial";
 import ScrollToTop from "@/components/ScrollToTop";
 
 const Home = () => {
@@ -14,6 +15,7 @@ const Home = () => {
       <AboutCommunity />
       <Cards />
       <FaqAccordion />
+      <TestimonialCarousel />
       <Footer />
     </>
   );
