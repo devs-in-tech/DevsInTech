2f0bb70f8db6c2c769098492c3e81a58dc5bc93d
Pranav Tripathi <tripathipranav14@gmail.com>
2023-06-28 22:13:59 +0530

    update: Change team data

--- src/components/TeamPage/index.jsx
+++ src/components/TeamPage/index.jsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import TeamData from "../content/team";
@@ -14,13 +13,7 @@ const TeamPage = () => {
           TeamData.map((data, index) => (
             <div key={index} className="p-4 md:w-72 w-72 sm:mb-0 mb-6">
               <div className="rounded-lg h-64 overflow-hidden">
-                <Image
-                  alt="content"
-                  width={500}
-                  height={500}
-                  className="object-cover object-center h-full w-full rounded-full border-8 border-[#AF7AF2]"
-                  src={data.img}
-                />
+                <img alt="content" className="object-cover object-center h-full w-full rounded-full border-8 border-[#AF7AF2]" src={data.img} />
               </div>
               <div className="relative bottom-10">
                 <div className="bg-[#2A1846] text-center p-2 rounded-t-2xl">
@@ -30,13 +23,13 @@ const TeamPage = () => {
 
                 <div className="bg-[#62DC67] p-3 rounded-b-2xl justify-center flex gap-3">
                   <Link href={data.GitHub} target="_blank">
-                    <FaGithub size={20} />
+                    <FaGithub size={24} className="hover:text-blue-700 transition-all transition-800"/>
                   </Link>
                   <Link href={data.LinkedIn} target="_blank">
-                    <FaLinkedin size={20} />
+                    <FaLinkedin size={24} className="hover:text-blue-700 transition-all transition-800"/>
                   </Link>
                   <Link href={data.Twitter} target="_blank">
-                    <FaTwitter size={20} />
+                    <FaTwitter size={24} className="hover:text-blue-700 transition-all transition-800"/>
                   </Link>
                 </div>
               </div>
