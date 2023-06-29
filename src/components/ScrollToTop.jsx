ad6d066b86a19ea30a10eec169eafdea2f1ee335
smty2018 <74114936+smty2018@users.noreply.github.com>
2023-06-29 18:13:39 +0530

    Update ScrollToTop.jsx

--- src/components/ScrollToTop.jsx
+++ src/components/ScrollToTop.jsx
@@ -12,7 +12,7 @@ const ScrollToTop = () => {
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
-      let height = 530;
+      let height = 580;
       const scrollCheck =
         document.documentElement.scrollTop || document.body.scrollTop;
       if (scrollCheck > height) {
