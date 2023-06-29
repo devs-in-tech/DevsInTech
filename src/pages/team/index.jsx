dd7800be16d11ccc1824f829a6bff5ca74b76fe4
smty2018 <74114936+smty2018@users.noreply.github.com>
2023-06-29 18:02:47 +0530

    Update index.jsx

--- src/pages/team/index.jsx
+++ src/pages/team/index.jsx
@@ -1,12 +1,14 @@
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 import TeamPage from "@/components/TeamPage";
+import ScrollToTop from "@/components/ScrollToTop";
 
 const Team = () => {
   return (
     <>
       <Navbar />
       <TeamPage />
+      <ScrollToTop />
       <Footer />
     </>
   );
