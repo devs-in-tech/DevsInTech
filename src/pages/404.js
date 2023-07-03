145248a2b8c30fa9564d9465254e650c06b7bee4
abhinav-m22 <abhinavpm05@gmail.com>
2023-07-03 17:36:44 +0530

    404 page update

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,8 @@ export default function custom404(){
             <div className="w-9/12 m-auto py-16 min-h-screen flex items-center justify-center">
                 <div className="shadow overflow-hidden sm:rounded-lg pb-8">
                     <div className="border-t border-gray-200 text-center pt-8">
-                        <h1 className="text-9xl font-bold text-red-500">404</h1>
-                        <h1 className="text-6xl font-medium py-8 text-white">oops! Page not found</h1>
+                        <h1 className="text-9xl font-bold text-white">404</h1>
+                        <h1 className="text-6xl font-medium py-8 text-white">Oops! Page not found</h1>
                         <p className="text-2xl pb-8 px-12 font-medium text-gray-300">
                             Oops! The page you are looking for does not exist. It might have been moved or deleted.
                             </p>
