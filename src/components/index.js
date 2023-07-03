91ab90e04a13464570ada6c7bfe0e90cf37ebef8
shraddha761 <106100728+shraddha761@users.noreply.github.com>
2023-07-03 21:59:08 +0530

    feat: lazyLoading is added

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,27 @@ export { default as HeroSection } from "./HeroSection";
 export { default as AboutCommunity } from "./HomePage/aboutCommunity";
 export { default as stats } from "./HomePage/stats";
 export { default as header } from "./header";
+
+
+const Hello = React.lazy(("./Hello"));
+const HeroSection = React.lazy(() => import("./HeroSection"));
+const AboutCommunity = React.lazy(() => import("./HomePage/aboutCommunity"));
+
+import React, { Suspense } from 'react';
+
+function App() {
+  return (
+    <div>
+      {/* Other components */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Hello />
+        <HeroSection />
+        <AboutCommunity />
+        <stats />
+        <header />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
