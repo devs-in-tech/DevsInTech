5bf95436a1a6fd34e9873c5c22d5e8c1c3358912
karthik <karthiknadar1204@gmail.com>
2023-07-09 13:05:37 +0530

    feat: make navbar translucent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,92 @@
+// import Image from "next/image";
+// import Link from "next/link";
+// import { useState } from "react";
+// import Logo from "../../public/logo2.png";
+// // import Button from "./button";
+// import HeaderSocialMedia from "./HeaderSocialMedia";
+
+// const Navbar = () => {
+//   let Links = [
+//     { name: "Home", link: "/" },
+//     { name: "About Us", link: "/#aboutCommunity" },
+//     { name: "Events", link: "/events" },
+//     { name: "Team", link: "/team" },
+//     { name: "FAQs", link: "/#faqs" },
+//     { name: "Contributors", link: "/Contributors" }
+//   ];
+//   let [open, setOpen] = useState(false);
+//   return (
+//     <div className="w-full z-999 top-0 left-0 sticky bg-black">
+//       <div className="md:flex md:items-center md:justify-between py-4 px-7">
+//         <div
+//           className="font-bold text-base cursor-pointer flex flex-col md:items-center font-secondary 
+//       text-white"
+//         >
+//           <span>
+//             <Image className="h-14 w-14" src={Logo} alt="" />
+//           </span>
+//           DevsInTech
+//         </div>
+
+//         <ul
+//           className={`md:flex md:items-center md:pb-0 px-20 md:px-0 pb-12 md:justify-center absolute md:static md:z-auto left-0 w-full transition-all duration-500 ease-in ${open ? "top-24 bg-black" : "top-[-550px]"
+//             }`}
+//         >
+//           {Links.map((link) => (
+//             <li
+//               key={link.name}
+//               className="md:ml-8 whitespace-nowrap text-xl md:my-0 my-7"
+//               onClick={() => setOpen(!open)}
+//             >
+//               <Link
+//                 href={link.link}
+//                 className="text-white font-3 hover:text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2] duration-200
+//                relative  transition-all duration-500 before:content-[''] before:absolute before:-bottom-2
+//                 before:left-0 before:w-0   before:h-1 before:opacity-0  before:transition-all before:duration-500
+//                 before:bg-gradient-to-r hover:before:w-full  hover:before:opacity-100
+//                 "
+//               >
+//                 {link.name}
+//               </Link>
+//             </li>
+//           ))}
+//           <li
+//             className="md:ml-8 whitespace-nowrap text-xl md:my-0 my-7"
+//             onClick={() => setOpen(!open)}
+//           >
+//             <Link
+//               href="https://discord.com/invite/g7FmxB9uZp"
+//               className="text-white font-3 hover:text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2] duration-200"
+//             >
+//               Join Us
+//             </Link>
+//           </li>
+//           <div className="md:hidden flex gap-4">
+//             <HeaderSocialMedia width={35} height={30} />
+//           </div>
+//         </ul>
+//         <div className="lg:flex lg:gap-5 hidden md:flex md:gap-3">
+//           <HeaderSocialMedia width={47} height={47} />
+//         </div>
+//       </div>
+//       <div
+//         onClick={() => setOpen(!open)}
+//         className="text-3xl absolute right-8 top-6 cursor-pointer md:hidden text-white"
+//       >
+//         <ion-icon name={open ? "close" : "menu"}></ion-icon>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 import Logo from "../../public/logo2.png";
-// import Button from "./button";
 import HeaderSocialMedia from "./HeaderSocialMedia";
 
 const Navbar = () => {
@@ -16,12 +100,9 @@ const Navbar = () => {
   ];
   let [open, setOpen] = useState(false);
   return (
-    <div className="w-full z-999 top-0 left-0 sticky bg-black">
+    <div className="w-full z-999 top-0 left-0 sticky bg-black bg-opacity-75 shadow-lg">
       <div className="md:flex md:items-center md:justify-between py-4 px-7">
-        <div
-          className="font-bold text-base cursor-pointer flex flex-col md:items-center font-secondary 
-      text-white"
-        >
+        <div className="font-bold text-base cursor-pointer flex flex-col md:items-center font-secondary text-white">
           <span>
             <Image className="h-14 w-14" src={Logo} alt="" />
           </span>
@@ -40,11 +121,7 @@ const Navbar = () => {
             >
               <Link
                 href={link.link}
-                className="text-white font-3 hover:text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2] duration-200
-               relative  transition-all duration-500 before:content-[''] before:absolute before:-bottom-2
-                before:left-0 before:w-0   before:h-1 before:opacity-0  before:transition-all before:duration-500
-                before:bg-gradient-to-r hover:before:w-full  hover:before:opacity-100
-                "
+                className="text-white font-3 hover:text-transparent bg-clip-text bg-gradient-to-r from-[#AF7AF2] via-[#A5F7A8] to-[#AF7AF2] duration-200 relative  transition-all duration-500 before:content-[''] before:absolute before:-bottom-2 before:left-0 before:w-0 before:h-1 before:opacity-0  before:transition-all before:duration-500 before:bg-gradient-to-r hover:before:w-full  hover:before:opacity-100"
               >
                 {link.name}
               </Link>
@@ -80,4 +157,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
