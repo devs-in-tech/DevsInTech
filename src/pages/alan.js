e62dc832a06fefe0912285d9c80a12b820b21094
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-07-21 18:24:22 +0530

    fix: visiblity in of alan ai

--- src/pages/alan.js
+++ src/pages/alan.js
@@ -1,12 +1,13 @@
 // alan-ai-setup.js
-import React, { useEffect } from 'react';
+import { useEffect } from "react";
 
-const AlanAIProjectKey = "f9b22abe4b61e7c364a5a62bdae65be32e956eca572e1d8b807a3e2338fdd0dc/stage";
+const AlanAIProjectKey =
+  "f9b22abe4b61e7c364a5a62bdae65be32e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 function AlanAIComponent() {
   useEffect(() => {
     // Load Alan AI script asynchronously when the component mounts on the client side
-    
+
     const additionalStyles = `
     .alanBtn-root {
       right: 46px !important;
@@ -14,29 +15,30 @@ function AlanAIComponent() {
     }
   `;
 
-  const styleTag = document.createElement('style');
-  styleTag.innerHTML = additionalStyles;
-  document.head.appendChild(styleTag);
+    const styleTag = document.createElement("style");
+    styleTag.innerHTML = additionalStyles;
+    document.head.appendChild(styleTag);
 
     // Initialize Alan AI and handle commands once the script is loaded
-       /* global alanBtn */
-      var alanBtnInstance = alanBtn({
-        key: AlanAIProjectKey,
-        onCommand: function (commandData) {
-          if (commandData && commandData.command === 'openURL') {
-            if (commandData.target === '_blank') {
-              window.open(commandData.url, '_newtab' + Math.floor(Math.random() * 999999));
-            } else {
-              window.location.href = commandData.url;
-            }
+    /* global alanBtn */
+    var alanBtnInstance = alanBtn({
+      key: AlanAIProjectKey,
+      onCommand: function (commandData) {
+        if (commandData && commandData.command === "openURL") {
+          if (commandData.target === "_blank") {
+            window.open(
+              commandData.url,
+              "_newtab" + Math.floor(Math.random() * 999999)
+            );
+          } else {
+            window.location.href = commandData.url;
           }
         }
-      });
- 
+      },
+    });
   }, []);
- 
 
-  return <div id="alan-btn"  />; // This div will serve as the mount point for the Alan AI button
+  return <div id="alan-btn" />; // This div will serve as the mount point for the Alan AI button
 }
 
 export default AlanAIComponent;
